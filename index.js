5801140d356bf6acab1fd429b3142ad0402ae3a2
Raj Krishnan R <rkrishnan1993@live.com>
2021-01-29 18:52:17 +0400

    ready to launch

--- index.js
+++ index.js
@@ -1,12 +1,57 @@
 const app = require('express')();
 const http=require('http').Server(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http,{
+  cors:{
+      origin:"*",
+      methods:["GET","POST"]
+  }  
+});
+
+var userList=[];
+
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/frame.html');
 });
 io.on('connection',(socket)=>{
 console.log('a user is connected');
+socket.on('register',(msg)=>{
+let user = new Object;
+user.id=msg;
+user.socketid=socket.id;
+userList.push(user);
+console.log(userList);
 });
+socket.on('request',(recipient)=>{
+for(var i =0;i<userList.length;i++)
+{
+    if(userList[i].id==recipient.id)
+    {
+        let package = new Object();
+        package.src=socket.id;
+        package.offer=recipient.offer;
+        io.to(userList[i].socketid).emit('offer',package);
+    }
+}
+});
+socket.on('answer',(package)=>{
+        package.src=socket.id;
+        package.answer=package.answer;
+        io.to(package.recipient).emit('answer',package);
+});
+socket.on('ice-candidate',(incpackage)=>{
+    for(var i =0;i<userList.length;i++)
+    {
+        if(userList[i].id==incpackage.recID)
+        {
+            let package = new Object();
+            package.src=socket.id;
+            package.candidate=incpackage.candidate;
+            io.to(userList[i].socketid).emit('ice-candidate',package);
+        }
+    } 
+});
+});
+
 http.listen(3000,'0.0.0.0',()=>{
     console.log('Listening on 3000');
 });
